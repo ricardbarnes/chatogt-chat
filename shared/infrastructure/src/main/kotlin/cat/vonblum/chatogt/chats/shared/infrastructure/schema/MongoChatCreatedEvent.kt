7dd67afc7a8b927084a25f6dc5d19c3a10d91774
Ricard Barnes <ricardbarnes@gmail.com>
2024-12-16 20:46:31 +0100

    wip

--- shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/chats/shared/infrastructure/schema/MongoChatCreatedEvent.kt
+++ shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/chats/shared/infrastructure/schema/MongoChatCreatedEvent.kt
@@ -1,7 +1,9 @@
 package cat.vonblum.chatogt.chats.shared.infrastructure.schema
 
+import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
+@Document(collection = "chat_created_event")
 class MongoChatCreatedEvent(
     id: String,
     aggregateId: String,
--- shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/chats/shared/infrastructure/schema/MongoEvent.kt
+++ shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/chats/shared/infrastructure/schema/MongoEvent.kt
@@ -1,10 +1,8 @@
 package cat.vonblum.chatogt.chats.shared.infrastructure.schema
 
 import org.springframework.data.annotation.Id
-import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
-@Document
 abstract class MongoEvent(
     @Id val id: String,
     val aggregateId: String,
