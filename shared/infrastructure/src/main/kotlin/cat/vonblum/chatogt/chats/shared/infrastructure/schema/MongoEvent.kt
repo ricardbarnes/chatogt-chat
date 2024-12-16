49c88bad02b631ade30859a6550f3fb588acd774
Ricard Barnes <ricardbarnes@gmail.com>
2024-12-16 21:06:03 +0100

    wip

--- shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/chats/shared/infrastructure/schema/MongoEvent.kt
+++ shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/chats/shared/infrastructure/schema/MongoEvent.kt
@@ -1,8 +1,11 @@
 package cat.vonblum.chatogt.chats.shared.infrastructure.schema
 
+import cat.vonblum.chatogt.chats.shared.domain.annotation.UsedBy
 import org.springframework.data.annotation.Id
 import java.time.Instant
 
+@Suppress("unused")
+@UsedBy("org.springframework.data")
 abstract class MongoEvent(
     @Id val id: String,
     val aggregateId: String,
