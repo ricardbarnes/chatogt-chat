6bd40d0eae7810889f63e5f33e38a40f3c7a125f
Ricard Barnes <ricardbarnes@gmail.com>
2024-12-16 21:03:43 +0100

    wip

---
+++ shared/domain/src/main/kotlin/cat/vonblum/chatogt/chats/shared/domain/annotation/UsedBy.kt
@@ -0,0 +1,4 @@
+package cat.vonblum.chatogt.chats.shared.domain.annotation
+
+@Suppress("unused")
+annotation class UsedBy(val value: String)
--- shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/chats/shared/infrastructure/schema/MongoChatCreatedEvent.kt
+++ shared/infrastructure/src/main/kotlin/cat/vonblum/chatogt/chats/shared/infrastructure/schema/MongoChatCreatedEvent.kt
@@ -1,9 +1,12 @@
 package cat.vonblum.chatogt.chats.shared.infrastructure.schema
 
+import cat.vonblum.chatogt.chats.shared.domain.annotation.UsedBy
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.Instant
 
 @Document(collection = "chat_created_event")
+@Suppress("unused")
+@UsedBy("@Document")
 class MongoChatCreatedEvent(
     id: String,
     aggregateId: String,
